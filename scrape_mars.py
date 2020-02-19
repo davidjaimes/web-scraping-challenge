581ba88d4815761eeaa4bcd80d747d418af50f20
David Jaimes <david@djaimes.com>
2020-02-19 15:22:02 -0800

    finish adding table

--- scrape_mars.py
+++ scrape_mars.py
@@ -6,25 +6,61 @@ import pandas as pd
 
 
 def scrape():
-    # Path to driver for macOS.
-    # Needed to serve content properly.
-    executable_path = {'executable_path': 'chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
+    try:
+        # Path to driver for macOS.
+        # Needed to serve content properly.
+        executable_path = {'executable_path': 'chromedriver'}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-    #  Visit NASA Mars News website with Chrome driver
-    # and parse html with Beautiful Soup.
-    url = 'https://mars.nasa.gov/news/'
-    browser.visit(url)
-    soup = BeautifulSoup(browser.html, 'html.parser')
+        #  Visit NASA Mars News website with Chrome driver
+        # and parse html with Beautiful Soup.
+        url = 'https://mars.nasa.gov/news/'
+        browser.visit(url)
+        soup = BeautifulSoup(browser.html, 'html.parser')
 
-    # Scrape and collect the latest news title and paragraph text.
-    news_title = soup.find('div', class_='content_title').text
-    news_p = soup.find('div', class_='article_teaser_body').text
+        # Scrape and collect the latest news title and paragraph text.
+        news_title = soup.find('div', class_='content_title').text
+        news_p = soup.find('div', class_='article_teaser_body').text
 
-    # Create Complete Dictionary
-    mars_data = {
-        'news_title': news_title,
-        'news_p': news_p
-    }
+        # Visit JPL Mars Space Images website with chrome driver
+        # and parse html with Beautiful Soup.
+        url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(url)
+        soup = BeautifulSoup(browser.html, 'html.parser')
 
-    return mars_data
+        # Scrape and collect the full size, image url for current Featured Mars Image.
+        base_url = 'https://www.jpl.nasa.gov'
+        image_url = soup.find('li', class_='slide').a['data-fancybox-href']
+        featured_image_url = base_url + image_url
+
+        # Visit Mars Weather twitter account with chrome driver
+        # and parse html with Beautiful Soup.
+        url = 'https://twitter.com/marswxreport?lang=en'
+        request = requests.get(url)
+        soup = BeautifulSoup(request.content, 'html.parser')
+
+        # Scrape and collect latest Mars weather tweet.
+        for find in soup.find_all('div', class_='js-tweet-text-container'):
+            if find.p.text[:11] == 'InSight sol':
+                mars_weather = find.p.text
+                break
+
+        # Visit the Mars Facts website with chrome driver.
+        url = 'https://space-facts.com/mars/'
+        browser.visit(url)
+        df = pd.read_html(browser.html)
+        html_table = df[0].to_html()
+
+        # Create Complete Dictionary
+        mars_data = {
+            'news_title': news_title,
+            'news_p': news_p,
+            'featured_image_url': featured_image_url,
+            'mars_weather': mars_weather,
+            'html_table': html_table
+        }
+
+        return mars_data
+
+    finally:
+        browser.quit()
